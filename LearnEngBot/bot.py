a8c1ad9d4f76eb864cb8a8ae999b7275b6ae3bb3
Maxim Shishkin <maximshishkin97@gmail.com>
2024-03-05 23:45:41 +0300

    FirstCommit

---
+++ LearnEngBot/bot.py
@@ -0,0 +1,35 @@
+﻿
+from webbrowser import get
+import telebot
+from getTask import * 
+
+bot = telebot.TeleBot('6976925865:AAG5rCOssPHXJbqwBiyLor8RZ13Snxeq4xQ')
+
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    helpStr="Привет, это проект LearnEngBot - Многофункциональный сервис для изучения английского языка. \n Для информации введите /help."
+    splitted_text = str(message.text).lower().split()
+    if str(message.text).lower() == "привет":
+        bot.send_message(message.from_user.id, helpStr)
+    elif str(message.text).lower() == "/help":
+        bot.send_message(message.from_user.id, "LearnEngBot - Многофункциональный сервис для изучения английского языка. \n Список команд: \n /s - вывести свой рейтинг \n /t - Получить задачу\n /a - ответ - дать ответ на задачу \n /h - Получить подсказку\n")
+    elif splitted_text[0] == "/t":
+        task, ans, taskid = getTask(message.from_user.id)
+        bot.send_message(message.from_user.id, task)    
+    elif splitted_text[0] == "/a":
+        task, ans, taskid = getTask(message.from_user.id)
+        str1="Ответ не верный"
+        if CheckAns(message.from_user.id,taskid, splitted_text[1]):
+            str1="Ответ верный"
+        bot.send_message(message.from_user.id, str1)
+    elif splitted_text[0] == "/h":
+        task, ans, taskid = getTask(message.from_user.id)
+        str1=getHelp(taskid)
+        bot.send_message(message.from_user.id, str1)
+    elif splitted_text[0] == "/s":
+        score = getScore(message.from_user.id)
+        bot.send_message(message.from_user.id, "Ваш рейтинг: "+ str(score))   
+    else:
+        bot.send_message(message.from_user.id, helpStr)
+
+bot.polling(none_stop=True, interval=0)
---
+++ LearnEngBot/getTask.py
@@ -0,0 +1,79 @@
+﻿import json
+from fuzzywuzzy import fuzz
+from fuzzywuzzy import process
+#pip install python-Levenshtein
+#pip install fuzzywuzzy
+# https://habr.com/ru/post/491448/
+
+def getTask(userid):
+    fileObject = open("tasks.json", "r", encoding="UTF-8")
+    jsonContent = fileObject.read()
+    aList = json.loads(jsonContent)
+    taskid=""
+    task=""
+    ans=""
+    for item in aList:
+        useridIsSolved=False
+        for i1 in item['solved']:
+            if i1==userid:
+                useridIsSolved=True
+                break
+        if useridIsSolved==False:
+            task = item['task']
+            ans=item['ans']
+            taskid=item['taskid']
+            break
+    return task, ans, taskid
+
+def getScore(userid):
+    fileObject = open("tasks.json", "r", encoding="UTF-8")
+    jsonContent = fileObject.read()
+    aList = json.loads(jsonContent)
+    score=0
+    for item in aList:
+        useridIsSolved=False
+        for i1 in item['solved']:
+            if i1==userid:
+                useridIsSolved=True
+                break
+        if useridIsSolved:
+            score+=item['score']
+    return score
+
+def CheckAns(userid,taskid, cans):
+    fileObject = open("tasks.json", "r", encoding="UTF-8")
+    jsonContent = fileObject.read()
+    aList = json.loads(jsonContent)
+    for item in aList:
+        if taskid==item['taskid']:
+            if fuzz.WRatio(cans, item['ans'])>85:
+            # if cans==item['ans']:  fuzz.WRatio(
+                item['solved'].append(userid)
+                jsonString = json.dumps(aList, ensure_ascii=False)
+                jsonFile = open("tasks.json", "w", encoding="UTF-8")
+                jsonFile.write(jsonString)
+                jsonFile.close()
+                return True   
+    return False
+
+def getHelp(taskid):
+    fileObject = open("tasks.json", "r", encoding="UTF-8")
+    jsonContent = fileObject.read()
+    aList = json.loads(jsonContent)
+    for item in aList:
+        if taskid==item['taskid']:
+            return item['help']   
+    return ""
+
+if __name__ == '__main__':
+    task, ans, taskid = getTask("123")
+    print(task)
+    print(ans)
+    print(taskid)
+    score = getScore("123")
+    print(score)
+    print(CheckAns("123","1", "14"))
+    task, ans, taskid = getTask("123")
+    print(task)
+    print(ans)
+    print(taskid)
